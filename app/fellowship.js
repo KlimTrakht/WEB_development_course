a8835495797ce9b23655bf2fa9072b2d5e65af2d
Klim Trakht <klim@kultprosvet.net>
2021-09-03 08:45:07 +0300

    Adds JS ES6 Lesson 2

---
+++ app/fellowship.js
@@ -0,0 +1,3 @@
+const fellowship = ['Frodo', 'Samwise', 'Gandalf'];
+const total = fellowship.length;
+export {fellowship, total};
---
+++ app/math.js
@@ -0,0 +1,11 @@
+const add = (a, b) => {
+  return a+b;
+}
+
+const multiply = (a, b) => {
+  return a*b;
+}
+
+export { add, multiply };
+
+export default multiply;
