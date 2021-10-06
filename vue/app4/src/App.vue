93db314aba7ba7ce2e429c3ddba0273c26b10f1f
Klim Trakht <klim@kultprosvet.net>
2021-10-06 10:23:31 +0300

    Adds Vue.js Lesson 5

--- vue/app4/src/App.vue
+++ vue/app4/src/App.vue
@@ -1,19 +1,66 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Got jokes ?</h1>
+    <button @click="initJokes" class="btn btn-primary">Init jokes</button>
+    <button @click="addJoke" class="btn btn-primary">Add joke</button>
+
+    <span @click="status = 'false'">Status: {{ status }}</span>
+
+    <br />
+    <span v-for="(type, index) in types" :key="'type-' + index">
+      <label>
+        <input type="checkbox" :value="type" v-model="checkedTypes" />
+        {{ type }}
+      </label>
+    </span>
+    <br />
+
+    <div class="col-md-12">
+      <div v-for="(joke, index) in jokes" :key="index" class="col-md-6">
+        <Joke
+          v-if="checkedTypes.includes(joke.type)"
+          :joke="joke"
+          :index="index"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { mapActions, mapGetters } from "vuex";
+import Joke from "@/components/Joke";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    Joke
+  },
+  data() {
+    return {
+      types: ["general", "knock-knock", "programming"],
+      checkedTypes: ["general", "knock-knock", "programming"]
+    };
+  },
+  computed: {
+    ...mapGetters({
+      jokes: "getJokes"
+    }),
+    status: {
+      get() {
+        return this.$store.getters["getStatus"];
+      },
+      set(val) {
+        this.$store.commit("setStatus", val);
+      }
+    }
+  },
+  methods: {
+    ...mapActions(["initJokes", "addJoke"])
+    // initJokes() {
+    //   this.$store.dispatch("initJokes");
+    // }
   }
-}
+};
 </script>
 
 <style lang="scss">
--- vue/app4/src/store/index.js
+++ vue/app4/src/store/index.js
@@ -1,15 +1,46 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    jokes: [],
+    status: "true"
+  },
+  getters: {
+    getJokes(state) {
+      return state.jokes;
+    },
+    getStatus(state) {
+      return state.status;
+    }
   },
   mutations: {
+    initJokes(state, payload) {
+      state.jokes.push(...payload);
+    },
+    addJoke(state, joke) {
+      state.jokes.push(joke);
+    },
+    removeJoke(state, index) {
+      state.jokes.splice(index, 1);
+    },
+    setStatus(state, status) {
+      state.status = status;
+    }
   },
   actions: {
+    initJokes({ commit }) {
+      fetch("http://localhost:3005/jokes/ten")
+        .then(response => response.json())
+        .then(json => commit("initJokes", json));
+    },
+    addJoke({ commit }) {
+      fetch("http://localhost:3005/jokes/random")
+        .then(response => response.json())
+        .then(json => commit("addJoke", json));
+    }
   },
-  modules: {
-  }
-})
+  modules: {}
+});
