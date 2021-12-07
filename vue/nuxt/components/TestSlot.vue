98e1584480943edb7b63119803f43e8a6d34fffb
MityaDv <mityadv@gmail.com>
2021-12-07 15:40:40 +0200

    learn slot

---
+++ vue/nuxt/components/TestSlot.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div>
+      <slot></slot>
+    </div>
+    <div>
+      <slot name="header"> </slot>
+    </div>
+    <div>
+      <slot name="subTitle"> </slot>
+    </div>
+    <div>
+      <slot name="for default"
+        >Значение по дефолту без указания тега в родителе</slot
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
--- vue/nuxt/pages/about.vue
+++ vue/nuxt/pages/about.vue
@@ -3,13 +3,47 @@
     <v-col class="text-center">
       <h1>About</h1>
       <nuxt-link to="/">Home</nuxt-link>
+      <test-slot class="mb-8">
+        <p>Устаревший синтаксис с использованием директивы "slot"</p>
+        <h2>По дефолту слот без имени</h2>
+        <h3 slot="header">Именнованный слот с именем header</h3>
+        <h4 slot="subTitle">Именнованный слот с именем subTitle</h4>
+      </test-slot>
+      <v-divider></v-divider>
+      <test-slot-new class="mt-8">
+        <template v-slot:default>
+          <p>Новый синтаксис с использованием директивы "v-slot"</p>
+          <h2>
+            По дефолту слот без имени (в template можно указывать двумя
+            способами v-slot или v-slot:default)
+          </h2>
+        </template>
+        <template v-slot:header>
+          <h3>
+            Именнованный слот с именем header (в template указываем
+            v-slot:header)
+          </h3>
+        </template>
+        <template #subTitle>
+          <h4>
+            Именнованный слот с именем subTitle (в template можно использовать
+            короткую запись #subTitle = v-slot:subTitle)
+          </h4>
+        </template>
+      </test-slot-new>
     </v-col>
   </v-row>
 </template>
 
 <script>
+import TestSlot from '@/components/TestSlot.vue'
+import TestSlotNew from '../components/TestSlotNew.vue'
 export default {
   name: 'AboutPage',
-  layout: 'nobar'
+  layout: 'nobar',
+  components: {
+    TestSlot,
+    TestSlotNew,
+  },
 }
 </script>
